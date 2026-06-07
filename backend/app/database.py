49afd821bc686356e0ad3c7f24a1441f7d1652de
Pooja-Gohel24 <poojajgohel2@gmail.com>
2026-06-08 01:10:11 +0530

    changes_done

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+db_url = settings.DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
+engine = create_engine(db_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
